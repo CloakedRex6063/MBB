53e1820cd1a84786bc191ae89ec09c80dc34be47
CloakedRex6063 <64742219+CloakedRex6063@users.noreply.github.com>
2022-12-15 23:33:14 +0530

    Level Select

--- Assets/Scroll.cs
+++ Assets/Scroll.cs
@@ -1,30 +1,27 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class Scroll : MonoBehaviour
 {
-    ScrollView scroll;
-    int currentelement=0;
-    List<VisualElement> levels;
-
-    private void Start()
-    {
-        scroll = GetComponentInChildren<ScrollView>();
-        for(int i=0; i<scroll.childCount; i++)
-        {
-            levels.Add(scroll.ElementAt(i));
-        }
-        scroll.ScrollTo(levels[0]);
-    }
+    public List<Sprite> screenShots;
+    public Image sprite;
+    public TextMeshProUGUI tmp;
+    private int curselect = 0;
+    
     // Start is called before the first frame update
     public void Next()
     {
-        scroll.ScrollTo(levels[currentelement++]);
+        curselect = Mathf.Clamp(curselect+1, 0, screenShots.Count-1);
+        tmp.text = (curselect+1).ToString();
+        sprite.sprite = screenShots[curselect];
     }
     public void Back()
     {
-        scroll.ScrollTo(levels[currentelement--]);
+        curselect = Mathf.Clamp(curselect-1, 0, screenShots.Count-1);
+        tmp.text = (curselect+1).ToString();
+        sprite.sprite = screenShots[curselect];
     }
 }
